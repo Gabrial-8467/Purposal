44f09306925ce5b70f9f3da193417f876bf42b96
gabrialdeora <141753923+Gabrial-8467@users.noreply.github.com>
2023-12-06 19:45:24 +0530

    script.js

--- script.js
+++ script.js
@@ -1,54 +1,85 @@
-document.addEventListener('DOMContentLoaded', function () {
-    function getRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+ document.addEventListener('DOMContentLoaded', function () {
+        let isDragging = false;
+        let offsetX, offsetY;
 
-    function createCard(message) {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.style.width = `${getRandomNumber(200, 300)}px`;
-        card.style.height = `${getRandomNumber(150, 250)}px`;
-        card.style.left = `${getRandomNumber(0, window.innerWidth - parseInt(card.style.width))}px`;
-        card.style.top = `${getRandomNumber(0, window.innerHeight - parseInt(card.style.height))}px`;
+        function getRandomNumber(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
 
-        const paragraph = document.createElement('p');
-        paragraph.textContent = message;
-        card.appendChild(paragraph);
+        function createCard(message) {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.style.width = `${getRandomNumber(200, 300)}px`;
+            card.style.height = `${getRandomNumber(150, 250)}px`;
+            card.style.left = `${getRandomNumber(0, window.innerWidth - parseInt(card.style.width))}px`;
+            card.style.top = `${getRandomNumber(0, window.innerHeight - parseInt(card.style.height))}px`;
 
-        card.addEventListener('mousedown', dragStart);
-        card.addEventListener('mouseup', dragEnd);
-        card.addEventListener('mousemove', dragMove);
+            const paragraph = document.createElement('p');
+            paragraph.textContent = message;
+            card.appendChild(paragraph);
 
-        document.body.appendChild(card);
-    }
+            card.addEventListener('mousedown', dragStart);
+            card.addEventListener('mouseup', dragEnd);
+            card.addEventListener('mousemove', dragMove);
 
-    function dragStart(e) {
-        isDragging = true;
-        offsetX = e.clientX - e.target.getBoundingClientRect().left;
-        offsetY = e.clientY - e.target.getBoundingClientRect().top;
-    }
+            // Mobile events
+            card.addEventListener('touchstart', touchStart);
+            card.addEventListener('touchend', touchEnd);
+            card.addEventListener('touchmove', touchMove);
 
-    function dragEnd() {
-        isDragging = false;
-    }
+            document.body.appendChild(card);
+        }
 
-    function dragMove(e) {
-        if (!isDragging) return;
+        function dragStart(e) {
+            isDragging = true;
+            offsetX = e.clientX - e.target.getBoundingClientRect().left;
+            offsetY = e.clientY - e.target.getBoundingClientRect().top;
+        }
 
-        const x = e.clientX - offsetX;
-        const y = e.clientY - offsetY;
+        function dragEnd() {
+            isDragging = false;
+        }
 
-        const card = e.target;
-        card.style.left = `${x}px`;
-        card.style.top = `${y}px`;
-    }
+        function dragMove(e) {
+            if (!isDragging) return;
 
-    createCard('You light up my world, would you go on a date with me? 💖');
-    createCard('You are extraordinary. Would you be my partner in crime? 😊');
-    createCard('You make every moment special. Will you be my girlfriend? ❤️');
-    createCard('Life is a journey, and I want you to be my travel companion. Will you? 🌍');
-    createCard('Your smile is contagious. Would you make me the happiest person and be mine? 😄');
-    createCard('In this crazy world, you\'re my constant. Will you be my forever? 🌟');
-    createCard('You\'re the missing piece to my puzzle. Can we complete it together? 🧩');
-    createCard('I don\'t believe in love at first sight, but your smile convinced me otherwise. Will you be mine? 😍');
-});
+            const x = e.clientX - offsetX;
+            const y = e.clientY - offsetY;
+
+            const card = e.target;
+            card.style.left = `${x}px`;
+            card.style.top = `${y}px`;
+        }
+
+        // Mobile functions
+        function touchStart(e) {
+            const touch = e.touches[0];
+            dragStart({
+                clientX: touch.clientX,
+                clientY: touch.clientY,
+                target: e.target,
+            });
+        }
+
+        function touchEnd() {
+            dragEnd();
+        }
+
+        function touchMove(e) {
+            const touch = e.touches[0];
+            dragMove({
+                clientX: touch.clientX,
+                clientY: touch.clientY,
+                target: e.target,
+            });
+        }
+
+        createCard('You light up my world, would you go on a date with me? 💖');
+        createCard('You are extraordinary. Would you be my partner in life? 😊');
+        createCard('You make every moment special. Will you be my girlfriend? ❤️');
+        createCard('Life is a journey, and I want you to be my travel companion. Will you? 🌍');
+        createCard('Your smile is contagious. Would you make me the happiest person and be mine? 😄');
+        createCard('In this crazy world, you\'re my constant. Will you be my forever? 🌟');
+        createCard('You\'re the missing piece to my puzzle. Can we complete it together? 🧩');
+        createCard('I don\'t believe in love at first sight, but your smile convinced me otherwise. Will you be mine? 😍');
+    });
